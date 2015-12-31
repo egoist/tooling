39e011beef33f007feb96db0b563580882cb627d
EGOIST <0x142857@gmail.com>
2015-12-31 17:06:12 +0800

    update postcss config

--- commands/watch.js
+++ commands/watch.js
@@ -5,7 +5,6 @@ const webpack = require('webpack')
 const server = require('../lib/server')
 const loadConfig = require('../lib/loadConfig')
 const open = require('open')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = function(options) {
 	const config = loadConfig('watch', options)
