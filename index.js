380f4a58c6b929418c1f082d1f1fe167c0e90397
EGOIST <0x142857@gmail.com>
2016-03-19 16:15:17 +0800

    make it modular

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 'use strict'
+const co = require('co')
+const readPkg = require('read-pkg-up')
+const globalNodeModules = require('global-node-modules')
+const npmin = require('npmin')
 
-module.exports = function main(type, options) {
-  const validTypes = ['build', 'watch']
-  if (validTypes.indexOf(type) < 0) {
-    throw new Error(`Unknown type, all valid types: ${validTypes.toString()}`)
+module.exports = co.wrap(function* (cli) {
+  // get entry from first arg or `entry` flag
+  const entry = cli.input[0] || cli.flags.entry
+  // get plugins
+  if (!cli.flags.use) {
+    throw new Error('Require plugins')
   }
-
-  require(`./commands/${type}`)(options)
-}
+  const plugins = Array.isArray(cli.flags.use) ? cli.flags.use : [cli.flags.use]
+  // get package.json in cwd
+  const localPkg = yield readPkg()
+  const self = {
+    entry,
+    output: cli.output,
+    flags: cli.flags,
+    pkg: localPkg,
+    tooling: cli.pkg
+  }
+  // install the plugins globally
+  npmin(plugins.map(name => `tooling-plugin-${name}`), {
+    global: true
+  })
+  // run runner
+  const runner = require(globalNodeModules(plugins[0]))
+  runner.call(self)
+})
